8f951d3ba40c5a4fbdb931c995cbfc738102be88
Jatin Chahar <jatinchahar008@gmail.com>
2025-03-19 14:04:27 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout<<"Changes completed done";
+  cout<<"Changes done correctly";
   return 0;
 }
